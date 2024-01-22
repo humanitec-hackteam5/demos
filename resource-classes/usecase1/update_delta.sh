28699b8a1bfd5ab8a7379356f51cb60c0fb5a405
Nikolay Kovalenko <nikolay.kovalenko@humanitec.com>
2024-01-22 16:27:21 +0100

    temp-creds

--- resource-classes/usecase1/update_delta.sh
+++ resource-classes/usecase1/update_delta.sh
@@ -1,3 +1,3 @@
-DELTA_ID=d9966feb33ac8252a6d52680f6e726df7399476c
-humctl update delta $DELTA_ID add /modules/sample-app/externals/secrets/class "sensitive"
-humctl update delta $DELTA_ID add /modules/sample-app/externals/public-files/class "external"
+DELTA_ID=c3b9e097d4c9dab885e0395e7b29ed941911228a
+humctl update delta $DELTA_ID add /modules/sample-app/externals/my-bucket/class "sensitive"
+#humctl update delta $DELTA_ID add /modules/sample-app/externals/public-files/class "external"
